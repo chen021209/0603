29963e5b8ecbfdb82fe3217f35cff9e128481259
chen021209 <150641254+chen021209@users.noreply.github.com>
2024-06-03 11:28:36 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -54,13 +54,57 @@ function draw() {
 
 function drawSkeleton() {
   // Draw all the tracked landmark points
-
-    partA = pose.keypoints[9];
-    partB = pose.keypoints[10];
+  for (let i = 0; i < poses.length; i++) {
+    pose = poses[i];
+    // shoulder to wrist
+    for (j = 5; j < 9; j++) {
+      if (pose.keypoints[j].score > 0.1 && pose.keypoints[j + 2].score > 0.1) {
+        partA = pose.keypoints[j];
+        partB = pose.keypoints[j + 2];
+        line(partA.x, partA.y, partB.x, partB.y);
+      }
+    }
+    // shoulder to shoulder
+    partA = pose.keypoints[5];
+    partB = pose.keypoints[6];
     if (partA.score > 0.1 && partB.score > 0.1) {
-      image(noiseImg,partA.x, partA.y, 50,50);
+      //line(partA.x,partA.y,partB.x,partB.y);
+      push()
+      image(noiseImg,partA.x-75, partA.y-75, 150,150);
+      image(noiseImg,partB.x-75, partB.y-75, 150,150); 
+      pop()     
       
     }
+    // hip to hip
+    partA = pose.keypoints[11];
+    partB = pose.keypoints[12];
+    if (partA.score > 0.1 && partB.score > 0.1) {
+      line(partA.x, partA.y, partB.x, partB.y);
+      
+    }
+    // shoulders to hips
+    partA = pose.keypoints[5];
+    partB = pose.keypoints[11];
+    if (partA.score > 0.1 && partB.score > 0.1) {
+      line(partA.x, partA.y, partB.x, partB.y);
+      
+    }
+    partA = pose.keypoints[6];
+    partB = pose.keypoints[12];
+    if (partA.score > 0.1 && partB.score > 0.1) {
+      line(partA.x, partA.y, partB.x, partB.y);
+      
+    }
+    // hip to foot
+    for (j = 11; j < 15; j++) {
+      if (pose.keypoints[j].score > 0.1 && pose.keypoints[j + 2].score > 0.1) {
+        partA = pose.keypoints[j];
+        partB = pose.keypoints[j + 2];
+        line(partA.x, partA.y, partB.x, partB.y);
+        
+      }
+    }
+  }
 }
 
 /* Points (view on left of screen = left part - when mirrored)
